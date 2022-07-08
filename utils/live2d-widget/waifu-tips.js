e1e466fcd3efb6c3a97fdf1a0b414a46e47f8929
gxf1212 <gxf1212@stu.xjtu.edu.cn>
2022-07-08 18:54:42 +0800

    remove git submodule

--- utils/live2d-widget/waifu-tips.js
+++ utils/live2d-widget/waifu-tips.js
@@ -44,7 +44,7 @@ function loadWidget(config) {
 		messageTimer,
 		messageArray = ["好久不见，日子过得好快呢……", "大坏蛋！你都多久没理人家了呀，嘤嘤嘤～", "嗨～快来逗我玩吧！", 
 						"拿小拳拳锤你胸口！", "记得把小家加入 Adblock 白名单哦！", 
-						"去B站搜索<a href='https://space.bilibili.com/441196634' 「东山月光下」</a>关注我哦"];
+						"去B站搜索<a href='https://space.bilibili.com/441196634'> 「东山月光下」 </a>关注我哦"];
 	window.addEventListener("mousemove", () => userAction = true);
 	window.addEventListener("keydown", () => userAction = true);
 	setInterval(() => {
